ab6bc31069ba02a992f0ae63afee58cf0ab73cfd
Sangeeth Nandakumar <twileloop@outlook.com>
2024-06-04 01:07:39 +0530

    fix - Excluded appsettings.json

--- ExpenceTracker/Installers/InfrastructureServiceInstaller.cs
+++ ExpenceTracker/Installers/InfrastructureServiceInstaller.cs
@@ -1,11 +1,21 @@
 ﻿
+using Twileloop.UOW.MongoDB.Support;
+
 namespace ExpenceTracker.Installers
 {
     public sealed class InfrastructureServiceInstaller : IServiceInstaller
     {
         public void InstallService(IServiceCollection services, IConfiguration configuration)
         {
-
+            //MongoDB
+            var connectionString = configuration.GetConnectionString("TrackerDB");
+            services.AddUnitOfWork((uow) =>
+            {
+                uow.Connections = new List<MongoDBConnection>
+                {
+                    new MongoDBConnection("ExpenceTracker", connectionString)
+                };
+            });
         }
     }
 }
